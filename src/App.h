02c83facc5265bab3c7673d92c0945c7037a649d
mitch092 <stevenm7861@gmail.com>
2019-08-18 01:01:17 -0700

    Tested lesson 2's shading rasterizer.

--- src/App.h
+++ src/App.h
@@ -2,15 +2,16 @@
 #include <chrono>  // For benchmarking the various draw_line versions.
 #include <stdexcept>
 #include <string>
+#include "Benchmark.h"
 #include "Error.h"
 #include "Frame.h"
 #include "Renderer.h"
 #include "SDL.h"
-#include "Benchmark.h"
 
 class App {
  public:
-  App(const char* name, int width, int height) : success(false), window(nullptr), screen(nullptr), error() {
+  App(const char* name, int width, int height, const char* _model)
+      : success(false), window(nullptr), screen(nullptr), model(Model{_model}), error() {
     success = SDL_Init(SDL_INIT_VIDEO) >= 0;
     if (success == false) error.throw_runtime_error("initialize SDL");
 
@@ -39,28 +40,28 @@ class App {
         if (e.type == SDL_QUIT) {
           quit = true;
         }
-      }
+      }  
       SDL_UpdateWindowSurface(window);
     }
   }
 
  private:
+
   void render() {
     Stopwatch watch;
     Renderer renderer{Frame{screen}};
 
     watch.reset_and_start();
-	//Model model(filename);
     //renderer.draw_wireframe(model);
-    renderer.draw_triangle_v6(glm::ivec2(10, 70), glm::ivec2(50, 160), glm::ivec2(70, 80), Color{255, 0, 0});
-    renderer.draw_triangle_v6(glm::ivec2(180, 50), glm::ivec2(150, 1), glm::ivec2(70, 180), Color{255, 255, 255});
-    renderer.draw_triangle_v6(glm::ivec2(180, 150), glm::ivec2(120, 160), glm::ivec2(130, 180), Color{0, 255, 0});
+    //renderer.draw_flat_rainbow_shaded_model(model);
+    renderer.draw_model_lighted(model);
     watch.stop_and_print();
   }
 
   bool success;
   SDL_Window* window;
   SDL_Surface* screen;
+  GLM_Model model;
 
   Error error;
 };
