f7311421262eb88514c4ab71de149ddc16add79d
mitch092 <stevenm7861@gmail.com>
2019-08-18 01:00:29 -0700

    Undid the enum class work on Color.h. Too much code for little gain.

--- src/Color.h
+++ src/Color.h
@@ -1,13 +1,8 @@
 #pragma once
 #include "SDL.h"
 
-//enum struct COLOR { red, green, blue, yellow, cyan, magenta, black, white, silver, grey };
-
 struct Color {
   Color(Uint8 _r, Uint8 _g, Uint8 _b) : r(_r), g(_g), b(_b) {}
   Color() : Color(0, 0, 0) {}
-
-  
-
   Uint8 r, g, b;
 };
