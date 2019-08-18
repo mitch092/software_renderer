35fdd05af05c11012a60fc35be47859c7c846682
mitch092 <stevenm7861@gmail.com>
2019-08-18 01:03:10 -0700

    Cleaned up some previous methods and added the flat light shader method.

--- src/Renderer.h
+++ src/Renderer.h
@@ -3,9 +3,10 @@
 #include <vector>
 #include "Color.h"
 #include "Frame.h"
+#include "GLM_Model.h"
+#include "Primitives.h"
 #include "SDL.h"
 #include "glm.hpp"
-#include "model.h"
 
 class Renderer {
  public:
@@ -56,37 +57,29 @@ class Renderer {
     }
   }
 
-  void draw_triangle_outline(const glm::ivec2& t0, const glm::ivec2& t1, const glm::ivec2& t2, const Color& color) {
-    draw_line(t0, t1, color);
-    draw_line(t1, t2, color);
-    draw_line(t2, t1, color);
+  void draw_triangle_outline(const RenderableTriangle& tri, const Color& color) {
+    draw_line(tri.a, tri.b, color);
+    draw_line(tri.b, tri.c, color);
+    draw_line(tri.c, tri.a, color);
   }
 
-  void draw_wireframe(Model& model) {
-    for (int i = 0; i < model.nfaces(); ++i) {
-      glm::vec3 v0 = model.vert(model.face(i)[0]);
-      glm::vec3 v1 = model.vert(model.face(i)[1]);
-      glm::vec3 v2 = model.vert(model.face(i)[2]);
-
-      glm::ivec2 a = vec3_to_ivec2(v0);
-      glm::ivec2 b = vec3_to_ivec2(v1);
-      glm::ivec2 c = vec3_to_ivec2(v2);
-
-      draw_triangle_outline(a, b, c, Color{255, 255, 255});
+  void draw_wireframe(GLM_Model& model) {
+    for (auto tri : model.get_renderable_triangles(frame.get_width(), frame.get_height())) {
+      draw_triangle_outline(tri, Color{255, 255, 255});
     }
   }
 
-  std::pair<glm::ivec2, glm::ivec2> find_bounding_box(const glm::ivec2& t0, const glm::ivec2& t1, const glm::ivec2& t2) {
+  Quad find_bounding_box(const RenderableTriangle& tri) {
     // Take care of the edge case in which the triangle is not in the frame at all.
-    if (!(frame.in_frame(t0.x, t0.y) && frame.in_frame(t1.x, t1.y) && frame.in_frame(t2.x, t2.y))) {
-      return {glm::ivec2(0, 0), glm::ivec2(0, 0)};
+    if (!(frame.in_frame(tri.a.x, tri.a.y) && frame.in_frame(tri.b.x, tri.b.y) && frame.in_frame(tri.c.x, tri.c.y))) {
+      return Quad{glm::ivec2(0, 0), glm::ivec2(-1, -1)};
     }
 
     // Find the bounding box around the triangle.
-    int left = std::min(t0.x, std::min(t1.x, t2.x));
-    int right = std::max(t0.x, std::max(t1.x, t2.x));
-    int bottom = std::min(t0.y, std::min(t1.y, t2.y));
-    int top = std::max(t0.y, std::max(t1.y, t2.y));
+    int left = std::min(tri.a.x, std::min(tri.b.x, tri.c.x));
+    int right = std::max(tri.a.x, std::max(tri.b.x, tri.c.x));
+    int bottom = std::min(tri.a.y, std::min(tri.b.y, tri.c.y));
+    int top = std::max(tri.a.y, std::max(tri.b.y, tri.c.y));
 
     // Now restrict it so that it's only in the frame.
     left = std::max(left, 0);
@@ -94,55 +87,50 @@ class Renderer {
     bottom = std::max(bottom, 0);
     top = std::min(top, frame.get_height() - 1);
 
-    return {glm::ivec2(left, bottom), glm::ivec2(right, top)};
+    return Quad{glm::ivec2(left, bottom), glm::ivec2(right, top)};
   }
 
-  glm::vec3 barycentric(const glm::ivec2& t0, const glm::ivec2& t1, const glm::ivec2& t2, const glm::ivec2& P) {
-    glm::vec3 a = glm::vec3(t2.x - t0.x, t1.x - t0.x, t0.x - P.x);
-    glm::vec3 b = glm::vec3(t2.y - t0.y, t1.y - t0.y, t0.y - P.y);
+  glm::vec3 barycentric(const RenderableTriangle& tri, const glm::ivec2& P) {
+    glm::vec3 a = glm::vec3(tri.c.x - tri.a.x, tri.b.x - tri.a.x, tri.a.x - P.x);
+    glm::vec3 b = glm::vec3(tri.c.y - tri.a.y, tri.b.y - tri.a.y, tri.a.y - P.y);
     glm::vec3 u = glm::cross(a, b);
     if (std::abs(u.z) < 1) return glm::vec3(-1, 1, 1);
     return glm::vec3(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
   }
 
-  void draw_triangle(const glm::ivec2& t0, const glm::ivec2& t1, const glm::ivec2& t2, const Color& color) {
-    auto box = find_bounding_box(t0, t1, t2);
-    for (int y = box.first.y; y <= box.second.y; ++y) {
-      for (int x = box.first.x; x <= box.second.x; ++x) {
-        auto bc_screen = barycentric(t0, t1, t2, glm::ivec2(x, y));
+  void draw_triangle(const RenderableTriangle& tri, const Color& color) {
+    auto box = find_bounding_box(tri);
+    for (int y = box.ll.y; y <= box.ur.y; ++y) {
+      for (int x = box.ll.x; x <= box.ur.x; ++x) {
+        auto bc_screen = barycentric(tri, glm::ivec2(x, y));
         if (bc_screen.x >= 0 && bc_screen.y >= 0 && bc_screen.z >= 0) frame.put_pixel(x, y, color);
       }
     }
   }
 
-  void draw_flat_rainbow_shaded_model(Model& model) {
-    for (int i = 0; i < model.nfaces(); ++i) {
-      glm::vec3 v0 = model.vert(model.face(i)[0]);
-      glm::vec3 v1 = model.vert(model.face(i)[1]);
-      glm::vec3 v2 = model.vert(model.face(i)[2]);
-      
-      glm::ivec2 a = vec3_to_ivec2(v0);
-      glm::ivec2 b = vec3_to_ivec2(v1);
-      glm::ivec2 c = vec3_to_ivec2(v2);
+  void draw_flat_rainbow_shaded_model(GLM_Model& model) {
+    for (auto tri : model.get_renderable_triangles(frame.get_width(), frame.get_height())) {
+      draw_triangle(tri, Color{(Uint8)(rand() % 255), (Uint8)(rand() % 255), (Uint8)(rand() % 255)});
+    }
+  }
 
-      draw_triangle(a, b, c, Color{(Uint8)(rand() % 255), (Uint8)(rand() % 255), (Uint8)(rand() % 255)});
+  void draw_model_lighted(GLM_Model& model) {
+    auto light_dir = glm::vec3(0, 0, -1);
+    auto screen_coord_triangles = model.get_renderable_triangles(frame.get_width(), frame.get_height());
+    auto world_coord_triangles = model.get_transformable_triangles();
+    for (int i = 0; i != world_coord_triangles.size(); ++i) {
+      auto tri1 = screen_coord_triangles[i];
+      auto tri2 = world_coord_triangles[i];
+      glm::vec3 n = glm::normalize(glm::cross(tri2.c - tri2.a, tri2.b - tri2.a));
+	  
+      float intensity = glm::dot(n, light_dir);
+      if (intensity > 0) {
+        Uint8 shade = static_cast<Uint8>(intensity * 255);
+        draw_triangle(tri1, Color{shade, shade, shade});
+	  }
     }
   }
 
  private:
-  glm::ivec2 vec3_to_ivec2(glm::vec3 vec) {
-    // OBJ model vertices are a triplet of vertices, each between -1 and 1.
-    // Trim off the z vertex.
-    auto vec2 = glm::vec2(vec.x, vec.y);
-    // Add 1 so that the vector is now between 0 and 2.
-    vec2 += glm::vec2(1.0f, 1.0f);
-    // Divide by 2 so that the vector is now between 0 and 1.
-    vec2 /= 2.0f;
-    // Multiply the coordinates so that they map onto the frame.
-    vec2.x *= frame.get_width();
-    vec2.y *= frame.get_height();
-    return glm::ivec2(vec2);
-  }
-
   Frame frame;
 };
