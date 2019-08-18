9e1728ae9982d4d631d9506268c9be9742ab4ccd
mitch092 <stevenm7861@gmail.com>
2019-08-18 00:59:42 -0700

    For now, the model is loaded in main again... but differently from before.

--- src/main.cpp
+++ src/main.cpp
@@ -4,7 +4,7 @@
 int main(int argc, char* argv[]) {
   try {
 
-    App app("tinyrenderer in SDL", 200, 200);
+    App app("tinyrenderer in SDL", 800, 800, "assets/african_head.obj");
     app.display();
 
   } catch (const std::runtime_error& error) {
