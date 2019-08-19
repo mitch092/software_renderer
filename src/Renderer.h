c1b2d768aac1da69b911afb820bc0aa2e45f2ec4
mitch092 <stevenm7861@gmail.com>
2019-08-18 23:18:56 -0700

    Added a screen clear function and fixed the bounding box.

--- src/Renderer.h
+++ src/Renderer.h
@@ -12,6 +12,14 @@ class Renderer {
  public:
   Renderer(Frame& _frame) : frame(_frame) {}
 
+  void clear_screen(const Color& color) {
+    for (int y = 0; y != frame.get_height(); ++y) {
+      for (int x = 0; x != frame.get_width(); ++x) {
+        frame.put_pixel(x, y, color);
+      }
+    }
+  }
+
   void draw_line(const glm::ivec2& a, const glm::ivec2& b, const Color& color) {
     int x0 = a.x;
     int y0 = a.y;
@@ -63,39 +71,19 @@ class Renderer {
     draw_line(tri.c, tri.a, color);
   }
 
+  void draw_quad_outline(const Quad& quad, const Color& color) {
+    auto bottom_right = glm::ivec2(quad.ur.x, quad.ll.y);
+    auto upper_left = glm::ivec2(quad.ll.x, quad.ur.y);
+    draw_line(quad.ll, bottom_right, color);
+    draw_line(bottom_right, quad.ur, color);
+    draw_line(quad.ur, upper_left, color);
+    draw_line(upper_left, quad.ll, color);
+  }
+
   void draw_wireframe(Model& model) {
     for (const auto& tri : model.get_renderable_triangles()) {
       draw_triangle_outline(tri, Color{255, 255, 255});
     }
-  }
-
-  Quad find_bounding_box(const RenderableTriangle& tri) {
-    // Take care of the edge case in which the triangle is not in the frame at all.
-    if (!(frame.in_frame(tri.a.x, tri.a.y) && frame.in_frame(tri.b.x, tri.b.y) && frame.in_frame(tri.c.x, tri.c.y))) {
-      return Quad{glm::ivec2(0, 0), glm::ivec2(-1, -1)};
-    }
-
-    // Find the bounding box around the triangle.
-    int left = std::min(tri.a.x, std::min(tri.b.x, tri.c.x));
-    int right = std::max(tri.a.x, std::max(tri.b.x, tri.c.x));
-    int bottom = std::min(tri.a.y, std::min(tri.b.y, tri.c.y));
-    int top = std::max(tri.a.y, std::max(tri.b.y, tri.c.y));
-
-    //Now restrict it so that it's only in the frame.
-    left = std::max(left, 0);
-    right = std::min(right, (frame.get_width() - 1));
-    bottom = std::max(bottom, 0);
-    top = std::min(top, frame.get_height() - 1);
-
-    return Quad{glm::ivec2(left, bottom), glm::ivec2(right, top)};
-  }
-
-  glm::vec3 barycentric(const RenderableTriangle& tri, const glm::ivec2& P) {
-    glm::vec3 a = glm::vec3(tri.c.x - tri.a.x, tri.b.x - tri.a.x, tri.a.x - P.x);
-    glm::vec3 b = glm::vec3(tri.c.y - tri.a.y, tri.b.y - tri.a.y, tri.a.y - P.y);
-    glm::vec3 u = glm::cross(a, b);
-    if (std::abs(u.z) < 1) return glm::vec3(-1, 1, 1);
-    return glm::vec3(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
   }
 
   void draw_triangle(const RenderableTriangle& tri, const Color& color) {
@@ -106,6 +94,8 @@ class Renderer {
         if (bc_screen.x >= 0 && bc_screen.y >= 0 && bc_screen.z >= 0) frame.put_pixel(x, y, color);
       }
     }
+    // For debugging the bounding_box.
+    // draw_quad_outline(box, Color{255, 0, 0});
   }
 
   void draw_flat_rainbow_shaded_model(Model& model) {
@@ -127,10 +117,39 @@ class Renderer {
       if (intensity > 0) {
         Uint8 shade = static_cast<Uint8>(intensity * 255);
         draw_triangle(tri1, Color{shade, shade, shade});
-	  }
+      }
     }
   }
 
  private:
+  Quad find_bounding_box(const RenderableTriangle& tri) {
+    // Take care of the edge case in which the triangle is not in the frame at all.
+    if (!frame.in_frame(tri.a.x, tri.a.y) && !frame.in_frame(tri.b.x, tri.b.y) && !frame.in_frame(tri.c.x, tri.c.y)) {
+      return Quad{glm::ivec2(0, 0), glm::ivec2(-1, -1)};
+    }
+
+    // Find the bounding box around the triangle.
+    int left = std::min(tri.a.x, std::min(tri.b.x, tri.c.x));
+    int right = std::max(tri.a.x, std::max(tri.b.x, tri.c.x));
+    int bottom = std::min(tri.a.y, std::min(tri.b.y, tri.c.y));
+    int top = std::max(tri.a.y, std::max(tri.b.y, tri.c.y));
+
+    // Now restrict it so that it's only in the frame.
+    left = std::max(left, 0);
+    right = std::min(right, (frame.get_width() - 1));
+    bottom = std::max(bottom, 0);
+    top = std::min(top, frame.get_height() - 1);
+
+    return Quad{glm::ivec2(left, bottom), glm::ivec2(right, top)};
+  }
+
+  glm::vec3 barycentric(const RenderableTriangle& tri, const glm::ivec2& P) {
+    glm::vec3 a = glm::vec3(tri.c.x - tri.a.x, tri.b.x - tri.a.x, tri.a.x - P.x);
+    glm::vec3 b = glm::vec3(tri.c.y - tri.a.y, tri.b.y - tri.a.y, tri.a.y - P.y);
+    glm::vec3 u = glm::cross(a, b);
+    if (std::abs(u.z) < 1) return glm::vec3(-1, 1, 1);
+    return glm::vec3(1.0f - (u.x + u.y) / u.z, u.y / u.z, u.x / u.z);
+  }
+
   Frame frame;
 };
