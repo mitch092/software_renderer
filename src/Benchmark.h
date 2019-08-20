4bb48e0b6ad3e2d482654e8f2d188b9cf0601c40
mitch092 <stevenm7861@gmail.com>
2019-08-19 22:36:25 -0700

    Made it so that the stopwatch prints fractions of a second.

--- src/Benchmark.h
+++ src/Benchmark.h
@@ -6,15 +6,17 @@ class Stopwatch {
   Stopwatch() = default;
   ~Stopwatch() = default;
 
-  void reset_and_start() {
-	  elapsed = std::chrono::system_clock::now(); 
-  }
+  void reset_and_start() { elapsed = std::chrono::system_clock::now(); }
   void stop_and_print() {
     auto end = std::chrono::system_clock::now();
+    auto duration = std::chrono::duration_cast<std::chrono::nanoseconds>(end - elapsed).count();
+    auto duration2 = static_cast<double>(duration);
+    duration2 /= 1000000000;
+
     std::string time;
     time.append("Elapsed time: ");
-    time.append(std::to_string(std::chrono::duration_cast<std::chrono::nanoseconds>(end - elapsed).count()));
-    time.append(" nanoseconds\n");
+    time.append(std::to_string(duration2));
+    time.append(" seconds\n");
     std::cerr << time;
   }
 
