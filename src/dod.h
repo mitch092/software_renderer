d91e632ea612644dfefcf12d3f300d98d7cdb1c4
mitch092 <stevenm7861@gmail.com>
2019-08-25 14:17:50 -0700

    Refining the data so that zbuffer entries map cleanly to color ids, even empty ones.

--- src/dod.h
+++ src/dod.h
@@ -11,22 +11,10 @@ struct Pixel {
   Color color;
 };
 
-struct Pixel2 {
-  float z;
-  Color color;
-};
-
 struct BarycentricCache {
   glm::vec3 v0, v1, tria;
   float invDenom;
 };
-
-BarycentricCache cache_triangle(const Triangle& tri) {
-  glm::vec3 v0 = tri.b - tri.a;
-  glm::vec3 v1 = tri.c - tri.a;
-  float invDenom = 1.0f / (v0.x * v1.y - v1.x * v0.y);
-  return {v0, v1, tri.a, invDenom};
-}
 
 glm::vec3 barycentric(const BarycentricCache& cache, const glm::ivec2& p) {
   glm::vec3 v2 = glm::vec3(p, 0) - cache.tria;
@@ -50,6 +38,7 @@ struct Data2 {
 
   // Lighting for each triangle. One light for now, maybe many more later.
   // Resize this to be the same size as triangles.
+  //
   std::vector<Color> shades;
 
   // Calculate whatever I can for barycentric coords using only the Triangles and store it here.
@@ -65,16 +54,25 @@ void calculate_normals(const std::vector<Triangle>& triangles, std::vector<glm::
 }
 
 void calculate_light(const std::vector<glm::vec3>& normals, std::vector<Color>& shades) {
+  shades.clear();
   for (int i = 0; i != normals.size(); ++i) {
     float intensity = glm::dot(normals[i], glm::vec3(0, 0, -1));
     Uint8 shade = static_cast<Uint8>(intensity * 255);
-    shades[i] = Color{shade, shade, shade};
+    shades.emplace_back(shade, shade, shade);
   }
+  // One extra color at the end (black), so that zbuffer_color_id can simply be initialized with shades.size(), 
+  // So blank pixels are set to black and branching is not needed in order to determine which pixels need to be black.
+  shades.emplace_back(255, 255, 255);
 }
 
 void calculate_bcaches(const std::vector<Triangle>& triangles, std::vector<BarycentricCache>& bcaches) {
+  bcaches.clear();
   for (int i = 0; i != triangles.size(); ++i) {
-    bcaches[i] = cache_triangle(triangles[i]);
+    const auto& tri = triangles[i];
+    glm::vec3 v0 = tri.b - tri.a;
+    glm::vec3 v1 = tri.c - tri.a;
+    float invDenom = 1.0f / (v0.x * v1.y - v1.x * v0.y);
+    bcaches.emplace_back(v0, v1, tri.a, invDenom);
   }
 }
 
@@ -95,34 +93,34 @@ struct Data {
   // I have no idea how to hardcode the number of pixels for each bounding box,
   // so I'll allow myself to use std::vector here.
   // Outer list: triangles. Inner list: pixels.
-  // ***ALL PIXELS IN BOX
-  std::vector<std::vector<glm::ivec2>> pixel_list;
+  // ***ALL PIXELS IN BOX FOR EVERY BOX
+  JaggedArray<glm::ivec2> pixel_list;
 
   // Each pixel in each bounding box gets mapped to a barycentric coordinate.
   // ***ALL BCOORDS IN BOX
-  std::vector<std::vector<glm::vec3>> bcoords;
+  JaggedArray<glm::vec3> bcoords;
 
   // Now create a list containing only the bcoords inside the triangle.
   // &&&ALL BCOORDS IN TRIANGLE
-  std::vector<std::vector<glm::vec3>> triangle_bcoords;
+  JaggedArray<glm::vec3> triangle_bcoords;
 
   // Now create a list containing only the pixels inside the triangle.
   // &&&ALL PIXEL X, Y COORDS IN TRIANGLE
-  std::vector<std::vector<glm::ivec2>> triangle_pixels;
+  JaggedArray<glm::ivec2> triangle_pixels;
 
   // Now calculate the z coordinates for every pixel inside of every triangle.
-  // &&&ALL PIXEL Z COORDS IN TRIANGLE
-  std::vector<std::vector<float>> triangle_z;
-
   // Every pixel on the screen stores a z index.
   // Index every glm::ivec2 from above, update the Z coord and the color if needed.
   // When it comes time to render: For every entry in this array,
   // if the Z is greater than std::numeric_limits<float>::min(),
   // then take that glm::ivec2 and the color and update the pixel list.
-  // Pixel2 zbuffer2[width * height];
+  // &&&ALL PIXEL Z COORDS IN TRIANGLE
+  JaggedArray<float> z_values_per_triangle;
 
-  // Outer list: x, Inner list: y
-  std::vector<std::vector<Pixel2>> zbuffer;
+  // &&&ALL Z COORDS IN THE SCREEN ZBUFFER
+  RectangularArray<float> zbuffer;
+  // &&&TRIANGLE/SHADE ARRAY INDEX ID FOR EACH FLOAT IN ZBUFFER
+  RectangularArray<int> zbuffer_color_id;
 
   // Every pixel to be drawn to the screen goes here.
   std::vector<Pixel> pixels;
