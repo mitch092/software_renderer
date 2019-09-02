e2f1463404cf2f55aa5a08756894ccd83a116ecf
mitch092 <stevenm7861@gmail.com>
2019-09-01 21:10:56 -0700

    Completely ripped out my version of std::optional. I'm just going to have to get it to work somehow.

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include <variant>
 #include "App.h"
+#include "dod/model.h"
 
 int main(int argc, char* argv[]) {
-  Maybe<Model> model = Model::get_model("african_head.obj");
-  if (model.tag == MAYBE::ABSENT) {
+  std::optional<std::ifstream> fstream = get_model_file_descriptor("african_head.obj");
+  if (!fstream.has_value()) {
     std::cerr << "Could not load the object file." << std::endl;
     return EXIT_FAILURE;
   }
 
-  auto app = App("tinyrenderer in SDL", 800, 800, model.val);
+  auto app = App("tinyrenderer in SDL", 800, 800, fstream.value());
   app.display();
 
   return EXIT_SUCCESS;
