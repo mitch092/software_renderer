ae949e2864707906ad5015e9c38e106b97a46a5b
mitch092 <stevenm7861@gmail.com>
2019-09-05 00:23:03 -0700

    Added a missing return statement and a todo note.

--- src/transforms.h
+++ src/transforms.h
@@ -10,12 +10,14 @@ glm::mat4 center_and_scale(int width, int height) {  // Add 1.
   auto scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.5f, 0.5f, 0.5f));
   // Multiply x and y by width and height. Not sure about z. Need to test with rectangular windows.
   auto scale2 = glm::scale(glm::mat4(1.0f), glm::vec3(width, height, (width + height) / 2));
+  return (scale2 * scale * translate);
 }
 
 glm::mat4 rotate(float delta_time, float radians) {
   float angle = radians * delta_time;
 
   // Save the current coordinates of the model. This is the point to rotate around.
+  // TODO: 400 400 400 are magic numbers. Fix later on.
   glm::mat4 translation = glm::translate(glm::mat4(1.0f), glm::vec3(400, 400, 400));
   // Apply the inverted coordinates in order to move the model to the origin.
   glm::mat4 invTranslation = glm::inverse(translation);
