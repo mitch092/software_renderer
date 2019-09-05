8617d08e2ac99686cfb33f0bdaa01c9b2262ef5f
mitch092 <stevenm7861@gmail.com>
2019-09-05 00:27:43 -0700

    Replaced ifstream with string everywhere.

--- src/Renderer.h
+++ src/Renderer.h
@@ -5,11 +5,11 @@
 
 class Renderer {
  public:
-  Renderer(std::ifstream& file, int width, int height) : data(std::unique_ptr<Data>(new Data{})) {
-    init_data(file, width, height, data.get());
+  Renderer(std::string& file, int width, int height) : data(std::unique_ptr<Data>(new Data{})) {
+    init_data(file, width, height, *data.get());
   }
 
-  void update(const glm::mat4& matrix) { update(matrix, data.get()); }
+  void update(const glm::mat4& matrix) { update_data(matrix, *data.get()); }
   void draw(Frame& frame) { draw_pixels(data.get()->pixels, frame); }
 
  private:
