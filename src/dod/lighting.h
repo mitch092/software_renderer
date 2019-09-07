729c6a41fa634a82918b19e9da0a802525254b30
mitch092 <stevenm7861@gmail.com>
2019-09-07 00:35:56 -0700

    Fixed an oversight that made the background pixels initialize to white and not black.

--- src/dod/lighting.h
+++ src/dod/lighting.h
@@ -8,12 +8,14 @@ void calculate_light(const std::vector<glm::vec3>& normals, const size_t& visibl
                      std::vector<Color>& shades) {
   for (size_t i = 0; i != visible_triangles_size; ++i) {
     float intensity = glm::dot(normals[i], glm::vec3(0, 0, -1));
+    //std::cout << intensity << std::endl;
     Uint8 shade = static_cast<Uint8>(intensity * 255);
     shades[i] = Color{shade, shade, shade};
+    //std::cout << normals[i].x << " " << normals[i].y << " " << normals[i].z << " " << std::endl;
   }
   // One extra color at the end (black), so that zbuffer_color_id can simply be initialized with shades.size() - 1,
   // So blank pixels are set to black and branching is not needed in order to determine which pixels need to be black.
   // shades.size() - 1 == visible_triangles_size, so I use the simpler constant value.
   assert(shades.size() - 1 == visible_triangles_size);
-  shades[visible_triangles_size] = Color{255, 255, 255};
+  shades[visible_triangles_size] = Color{0, 0, 0};
 }
