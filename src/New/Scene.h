c8bbc475ad11bf28ec3cdddd10cb3a6b99b576cd
mitch092 <stevenm7861@gmail.com>
2020-01-12 18:13:31 -0800

    Made it so that simple scenes are easy to create.

--- src/New/Scene.h
+++ src/New/Scene.h
@@ -1,14 +1,30 @@
 #pragma once
 
+#include <glm/glm.hpp>
 #include <vector>
 #include "Transform.h"
 
-
 struct Scene {
-  float far, near, fov;
-  glm::mat4 proj;
+  Scene(float _far, float _near, float _fov, int _width, int _height)
+      : far(_far),
+        near(_near),
+        fov(_fov),
+        width(_width),
+        height(_height),
+        proj(glm::perspective(fov, (static_cast<float>(width) / static_cast<float>(height)), near, far)) {}
 
-  glm::vec4 viewport;
+  void addModel(Transform _transform, std::vector<glm::uvec3> _faces, std::vector<glm::vec3> _verts) {
+    modelTransforms.push_back(_transform);
+    faces.push_back(_faces);
+    verts.push_back(_verts);
+    transformedVerts.push_back(_verts);
+  }
+  void addCamera(Transform camera) { cameras.push_back(camera); }
+  void addLight(glm::vec3 light) { directionalLights.push_back(light); }
+
+  const float far, near, fov;
+  int width, height;
+  const glm::mat4 proj;
 
   std::vector<Transform> modelTransforms;
   // Outer vector: models, inner vector: faces.
@@ -16,7 +32,7 @@ struct Scene {
   std::vector<std::vector<glm::vec3>> verts;
   std::vector<std::vector<glm::vec3>> transformedVerts;
 
-  std::vector<Transform> cameraTransforms;
+  std::vector<Transform> cameras;
 
   std::vector<glm::vec3> directionalLights;
 };
